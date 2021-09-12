ac422084d003c720183ffbfaa4823e23a902920f
Megha <megha.agarwal@somaiya.edu>
2021-09-12 16:46:10 +0530

    commit to main

--- Check_Website_Status/first/views.py
+++ Check_Website_Status/first/views.py
@@ -1,27 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import websiteurl
-import urllib 
+import urllib
 
 
 def home(request):
-    return render(request,'first/home copy.html')
+    return render(request, 'first/template1.html')
+
 
 def check(request):
-    web = request.GET.get('url','default')
+    web = request.GET.get('url', 'default')
     if 'https://' not in web:
         web = 'https://'+web
     try:
         status = urllib.request.urlopen(web).getcode()
-        website_url = websiteurl(web_name=web,status='working')
+        website_url = websiteurl(web_name=web, status='working')
         website_url.save()
         return HttpResponse('WORKING')
     except:
-        website_url = websiteurl(web_name=web,status='not-working')
+        website_url = websiteurl(web_name=web, status='not-working')
         website_url.save()
         return HttpResponse('NOTWORKING')
-
-    
-    
-    
-    
