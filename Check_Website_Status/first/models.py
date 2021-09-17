c5cd0435aa734147fdedfe44489319c1203c382e
NityaGada <87537845+NityaGada@users.noreply.github.com>
2021-09-17 16:26:02 +0530

    Co-authored-by: Rohan Pravin Chavan <RohanChavan07@users.noreply.github.com> Co-authored-by: yashvidonga <yashvidonga@users.noreply.github.com> Co-authored-by: suhani0330 <suhani0330@users.noreply.github.com> Co-authored-by: pratyushd02 <pratyushd02@users.noreply.github.com>

--- Check_Website_Status/first/models.py
+++ Check_Website_Status/first/models.py
@@ -5,6 +5,7 @@ class websiteurl(models.Model):
     url_id = models.AutoField(primary_key=True)
     web_name = models.CharField(max_length=50, default="")
     status = models.CharField(max_length=20, default="")
+    status_code = models.CharField(max_length=20, default="")
 
     def __str__(self):
         return self.web_name
@@ -12,3 +13,4 @@ class websiteurl(models.Model):
 
 class FilesUpload(models.Model):
     file = models.FileField()
+
--- Check_Website_Status/first/views.py
+++ Check_Website_Status/first/views.py
@@ -13,22 +13,35 @@ def check(request):
     web = request.GET.get('url', 'default')
     if 'https://' not in web:
         web = 'https://'+web
+    website_dict = {"website_name": web, "status": "", "code": 200}
     try:
         status = urllib.request.urlopen(web).getcode()
-        website_url = websiteurl(web_name=web, status='working')
+        website_dict["status"] = "Working"
+        website_dict["code"] = status
+        website_url = websiteurl(web_name=web, status='working', status_code = status)
         website_url.save()
-        return HttpResponse('WORKING')
-    # except urllib.error.HTTPError as e:
+        return render(request,'first/response.html', {'website_dict': website_dict, 'flag': 1})
+    except urllib.error.HTTPError as e:
+        website_dict["status"] = "Not-Working"
+        website_dict["code"] = e.code
+        website_url = websiteurl(web_name=web, status='not-working', status_code = e.code)
+        website_url.save()
     #     # Return code error (e.g. 404, 501, ...)
-    #     return HttpResponse('NOTWORKING = {}'.format(e.code))
+        return render(request,'first/response.html', {'website_dict': website_dict, 'flag': 1})
 
-    # except urllib.error.URLError as e:
-    #     # Not an HTTP-specific error (e.g. connection refused)
-    #     return HttpResponse('URL Error NOTWORKING {}'.format(e.code))
-    except:
-        website_url = websiteurl(web_name=web, status='not-working')
+    except urllib.error.URLError as e:
+        website_dict["status"] = "Not-Working"
+        website_dict["code"] = e.code
+        website_url = websiteurl(web_name=web, status='not-working', status_code = e.code)
         website_url.save()
-        return HttpResponse('NOTWORKING')
+    #     # Not an HTTP-specific error (e.g. connection refused)
+        return render(request,'first/response.html', {'website_dict': website_dict, 'flag': 1})
+    except:
+        website_dict["status"] = "Not-Working"
+        website_dict["code"] = status
+        website_url = websiteurl(web_name=web, status='not-working', status_code = status)
+        website_url.save()
+        return render(request,'first/response.html', {'website_dict': website_dict,'flag': 1})
 
 
 def upload(request):
@@ -41,8 +54,12 @@ def upload(request):
         for i in df.iloc[:,0]:
             try:
                 status = urllib.request.urlopen(i).getcode()
-                dic[i]="WORKING"
+                dic[i]="Working"
+            except urllib.error.HTTPError as e:
+                dic[i]='Not-Working : {}'.format(e.code)
+            except urllib.error.URLError as e:
+                dic[i]='Not-Working :{}'.format(e.code)
             except:
-                dic[i]="NOT-WORKING"
-        return render(request, 'first/template1.html',{'dic':dic})
-    return render(request, 'first/template1.html')
+                dic[i]="Not-Working"
+        return render(request, 'first/response.html',{'dic':dic , 'flag': 2})
+    return render(request, 'first/response.html')
